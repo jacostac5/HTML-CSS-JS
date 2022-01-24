e50357fd84dca7cc3d41600e73c2a84ee8cc231c
jacostac5 <85777249+jacostac5@users.noreply.github.com>
2022-01-24 13:22:17 -0500

    Add files via upload

--- Trabajo de investigacion/js/app.js
+++ Trabajo de investigacion/js/app.js
@@ -9,29 +9,31 @@ function toggleMenu() {
 openMenuBtn.addEventListener("click", toggleMenu);
 closeMenuBtn.addEventListener("click", toggleMenu);
 
-const menuLinks = document.querySelectorAll('.menu a[href^="#"]'); //empieza
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-const id = entry.target.getAttribute("id");
-const menuLink = document.querySelector(`.menu a[href="#${id}"]`); //igual
+const menuLink = document.querySelectorAll('.menu a[href^="#"]');
 
-if(entry.isIntersecting) {
-  document.querySelector(".menu a.selected").classList.remove("selected")
-  menuLink.classList.add("selected"); // pone la linea inferior en la seleccion
-}
-})
-}, {rootMargin: "-50% 0px -50% 0px"}) // margen alrededor o interior de ese elemento.
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      const id = entry.target.getAttribute("id");
+      const menuLink = document.querySelector(`.menu a[href="#${id}"]`);
 
-// remover menu en el movil al seleccionar una opcion
-menuLinks.forEach(menuLink => {
-  menuLink.addEventListener("click", function() {
+      if (entry.isIntersecting) {
+        document.querySelector(".menu a.selected").classList.remove("selected");
+        menuLink.classList.add("selected");
+      }
+    });
+  },
+  { rootMargin: "-50% 0px -50% 0px" }
+);
+
+menuLink.forEach((menuLink) => {
+  menuLink.addEventListener("click", function () {
     menu.classList.remove("menu_opened");
-  })
+  });
 
-  const hash = menuLink.getAttribute("href"); // seleccionar el elemnto q deseamos aplicar la linea
-  const target = document.querySelector(hash); // lo aplica
-  if(target) {
+  const hash = menuLink.getAttribute("href");
+  const target = document.querySelector(hash);
+  if (target) {
     observer.observe(target);
   }
-
-})
+});
